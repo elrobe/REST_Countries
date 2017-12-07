6ba0a4ad1967a4c0298d83e9312a62bf8ce219c8
elrobe <eliasroberg@gmail.com>
2017-12-07 00:18:45 -0500

    Added some extra documentation to start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-sudo php -S localhost:80 -t ./src
+#
+# File:     start.sh
+# Author:   Eli Berg
+# Purpose:  Quick script to start the PHP server using the src/ directory
+#           as the document root. 
+#
+
+# [-S localhost:80] will run the PHP server on the local machine on port 80
+# [-t ./src] will set the src/ directory as the document root
+sudo php -S localhost:80 -t src/
